297768392382c06f8acf9b9f93e8989a22352bcc
Allan Kimmer Jensen <hi@akj.io>
2025-10-27 04:54:01 +0000

    feat(storage): add centralized storage exports
    - Create index.ts to re-export all storage modules
    - Provides single import point for storage functionality
    - Organizes exports by purpose (types, filters, CRUD, queries, etc.)
    - Enables clean imports: import { saveMission } from 'lib/storage'

---
+++ extension/src/lib/storage/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Storage library - Centralized exports
+ * All storage functions organized by purpose
+ */
+
+// Types
+export * from './types';
+
+// Automation filters
+export * from './getAutomationFilters';
+export * from './useAutomationFilters';
+
+// Mission CRUD
+export * from './missions';
+
+// Mission queries and filtering
+export * from './missionQueries';
+
+// Mission statistics
+export * from './missionStats';
+
+// User options
+export * from './userOptions';
+
+// Storage statistics
+export * from './storageStats';
+
+// DOM utilities
+export * from './domUtils';
