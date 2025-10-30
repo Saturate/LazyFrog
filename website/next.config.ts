cc955ade0040447aa6028659832e3c43e0d01215
Allan Kimmer Jensen <hi@akj.io>
2025-10-30 01:12:51 +0100

    Update next.config.ts

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@lazyfrog/types'],
+  transpilePackages: ['@lazyfrog/types', 'react-apexcharts', 'apexcharts'],
 };
 
 export default nextConfig;
