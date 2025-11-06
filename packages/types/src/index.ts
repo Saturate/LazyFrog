d252976775002acef5bdfb6ecd7eceff4d672485
Allan Kimmer Jensen <hi@akj.io>
2025-11-06 11:25:00 +0100

    Update index.ts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -63,6 +63,7 @@ export interface AbilityChoiceEncounter {
   isEnchanted: boolean;
   optionA: Effect;
   optionB: Effect;
+  optionC?: Effect; // Optional third choice
 }
 
 export interface Enemy {
